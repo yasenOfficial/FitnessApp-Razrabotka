e2bd1d6f5a360951aa570746f69a34dac344b84a
yasenOfficial <yasentsv@gmail.com>
2025-05-27 21:38:51 +0300

    main.py tests pass

--- routes/main.py
+++ routes/main.py
@@ -1,4 +1,5 @@
 from flask import redirect, render_template, send_from_directory
+from flask_jwt_extended import get_jwt_identity, verify_jwt_in_request
 
 from utils.helpers import get_current_user
 
@@ -7,9 +8,13 @@ from . import main_bp
 
 @main_bp.route("/")
 def index():
-    user = get_current_user()
-    if user:
-        return redirect("/dashboard")
+    try:
+        verify_jwt_in_request()
+        user_id = get_jwt_identity()
+        if user_id:
+            return redirect("/dashboard")
+    except:
+        pass
     return render_template("home.html")
 
 
